c9cebdddb843e1249c129c45c3af81ce75d0b85f
tlynn <tlynn@users.noreply.github.com>
2017-06-22 23:41:19 +0100

    Move all the global variables into a `platform` object.

--- html5/nifki.js
+++ html5/nifki.js
@@ -1,16 +1,18 @@
 "use strict";
 
-var ctx = null;
-{
-    var canvas = document.getElementById('game');
-    if (canvas.getContext) {
-        ctx = canvas.getContext('2d');
-        ctx.mozImageSmoothingEnabled = false;
-        ctx.webkitImageSmoothingEnabled = false;
-        ctx.msImageSmoothingEnabled = false;
-        ctx.imageSmoothingEnabled = false;
-        ctx.scale(canvas.width, canvas.height);
-    }
+/** Returns a 2D drawing context (a CanvasRenderingContext2D) for rendering
+ * the graphics. */
+function initCanvas(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    if (!canvas.getContext) return null;
+    var ctx = canvas.getContext("2d");
+    if (!ctx) return null;
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.msImageSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+    ctx.scale(canvas.width, canvas.height);
+    return ctx;
 }
 
 function rgb(r, g, b) {
@@ -21,8 +23,9 @@ function rgb(r, g, b) {
     return "rgb(" + R + "," + G + "," + B + ")";
 }
 
-function render(state) {
-    ctx.save()
+function render(platform, state) {
+    var ctx = platform.context2d;
+    ctx.save();
     var win = state.window;
     ctx.fillStyle = rgb(win.r, win.g, win.b);
     ctx.fillRect(0.0, 0.0, 1.0, 1.0);
@@ -31,27 +34,31 @@ function render(state) {
     for (var i=0; i < state.images.length; i++) {
         ctx.drawImage(state.images[i], 32*i, 32*i);
     }
-    ctx.restore()
+    ctx.restore();
 }
 
-function frame(state) {
+function frame(platform, state) {
     console.log("frame()");
     // Run the interpreter here.
     state.window.r += 0.01;
     var terminated = state.window.r >= 0.8;
     if (terminated) {
         // Nothing else to do. Stop the timer.
-        clearInterval(intervalId);
+        clearInterval(platform.intervalId);
     } else {
         // Draw a frame and wait to be called again.
-        render(state);
+        render(platform, state);
     }
 }
 
-var intervalId = null;
-
-function run(images, properties) {
+function run(images, properties, canvasId) {
     console.log("run()");
+    var context2d = initCanvas(canvasId);
+    if (!context2d) throw "2D canvas is not available";
+    var platform = {
+        "context2d": context2d,
+        "intervalId": null
+    };
     var state = {
         "images": images,
         "window": {
@@ -59,24 +66,26 @@ function run(images, properties) {
             "x": 0, "y": 0, "w": properties.w, "h": properties.h
         }
     };
-    if (ctx !== null) {
-        intervalId = setInterval(frame, properties.msPerFrame, state)
-        frame(state);
-    }
+    platform.intervalId = setInterval(
+        frame,
+        properties.msPerFrame,
+        platform,
+        state
+    );
 }
 
-function loadImagesThen(image_filenames, run) {
+function loadImagesThen(image_filenames, callback) {
     var counter = image_filenames.length;
     var images = [];
 
     function count() {
         counter -= 1;
         if (counter == 0) {
-            run(images);
+            callback(images);
         }
     }
 
-    for (var i=0; i<image_filenames.length; i++) {
+    for (var i=0; i < image_filenames.length; i++) {
         var image = new Image();
         image.onload = count;
         image.src = image_filenames[i];
@@ -86,13 +95,11 @@ function loadImagesThen(image_filenames, run) {
 }
 
 function onload() {
-    var properties = {
-        w: 256, h: 256,
-        msPerFrame: 40
-    }
     loadImagesThen(
         ["man.png", "boulder.png"],
-        function(images) { run(images, properties); }
+        function(images) {
+            run(images, {"w": 256, "h": 256, "msPerFrame": 40}, "game");
+        }
     );
 }
 
